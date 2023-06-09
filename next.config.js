529c43ec766bc54f4fc41786bcc68c8261294186
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-09 10:13:16 +0530

    Finalized Projects

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
   },
-  nextAuth: {
-    providers: [
-      {
-        name: "facebook",
-        config: {
-          clientId: "643328173891346",
-          clientSecret: "2de1a21436f7d162c3cd4e0c3948536f",
-        },
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
