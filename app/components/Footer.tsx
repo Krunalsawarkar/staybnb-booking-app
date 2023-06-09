e4e3d151856bd2d6e5c1e0bce8711c10b2c8b9df
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-09 15:24:48 +0530

    Finalized Project

---
+++ app/components/Footer.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { FiHeart } from "react-icons/fi";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col items-center md:flex-row md:justify-between">
+          <div className="text-gray-600 text-center md:text-left">
+            Staybnb {new Date().getFullYear()} © All rights reserved.
+          </div>
+          <div className="flex items-center mt-2 md:mt-0">
+            <span className="mr-2">Made with</span>
+            <div className="hidden md:block">
+              <FiHeart className="text-red-500" />
+            </div>
+            <div className="md:hidden">
+              <FiHeart className="text-red-500 inline-block align-middle" />
+            </div>
+            <span className="ml-2">by Krunal Sawarkar</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
 import SearchModal from "./components/modals/SearchModal";
+import Footer from "./components/Footer";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default async function RootLayout({
         >
           {children}
         </div>
+        <Footer/>
       </body>
     </html>
   );
